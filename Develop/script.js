7972d5b8afc364123db53ac45dc3fbac55d538a8
Ali Rudnick <ali.rudnick@yahoo.com>
2022-11-06 12:11:25 -0600

    adding HTML, some starter css and declaring JS variables

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+//add click listener to View Highscores button
+
+//declare global variables
+var header = document.getElementById("header");
+var score = document.getElementById("score");
+var timer = document.getElementById("timer");
+var Intro = document.getElementById("Intro");
+var start-btn = document.getElementById("StartBtn");
+var questions = document.getElementById("questions");
+var questionHere = document.getElementById("questionHere");
+var answers = document.getElementById("answers");
+var OptionA = document.getElementById("OptionA");
+var OptionB = document.getElementById("OptionB");
+var OptionC = document.getElementById("OptionC");
+var OptionD = document.getElementById("OptionD");
